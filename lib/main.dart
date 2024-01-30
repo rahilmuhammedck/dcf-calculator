39e23bf05cc28b5e33611c920aa4f4bcbe9b4628
Muhammed Rahil CK <139740420+rahilmuhammedck@users.noreply.github.com>
2024-01-30 23:21:51 +0530

    update main added icon to assets but not reflected

--- lib/main.dart
+++ lib/main.dart
@@ -8,39 +8,128 @@ class DCFCalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DCF Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DCFCalculatorScreen(),
+      home: DCFInputScreen(),
     );
   }
 }
 
-class DCFCalculatorScreen extends StatefulWidget {
+class DCFInputScreen extends StatefulWidget {
   @override
-  _DCFCalculatorScreenState createState() => _DCFCalculatorScreenState();
+  _DCFInputScreenState createState() => _DCFInputScreenState();
 }
 
-class _DCFCalculatorScreenState extends State<DCFCalculatorScreen> {
-  // Add your state variables and logic here
+class _DCFInputScreenState extends State<DCFInputScreen> {
+  TextEditingController netIncomeController = TextEditingController();
+  TextEditingController depreciationController = TextEditingController();
+  TextEditingController amortizationController = TextEditingController();
+  TextEditingController capExController = TextEditingController();
+  TextEditingController workingCapitalController = TextEditingController();
+  TextEditingController costOfEquityController = TextEditingController();
+  TextEditingController costOfDebtController = TextEditingController();
+  TextEditingController taxRateController = TextEditingController();
+  TextEditingController terminalGrowthRateController = TextEditingController();
+  TextEditingController forecastYearsController = TextEditingController();
+  TextEditingController exitMultipleController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DCF Calculator'),
+        title: Text('DCF Input Screen'),
       ),
-      body: Center(
-        // Add your UI components here
-        child: Text(
-          'Welcome to DCF Calculator!',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildTextField("Net Income", netIncomeController),
+            buildTextField("Depreciation", depreciationController),
+            buildTextField("Amortization", amortizationController),
+            buildTextField("Capital Expenditures", capExController),
+            buildTextField(
+                "Changes in Working Capital", workingCapitalController),
+            buildTextField("Cost of Equity", costOfEquityController),
+            buildTextField("Cost of Debt", costOfDebtController),
+            buildTextField("Tax Rate", taxRateController),
+            buildTextField(
+                "Terminal Growth Rate", terminalGrowthRateController),
+            buildTextField(
+                "Number of Years in Forecast", forecastYearsController),
+            buildTextField("Exit Multiple", exitMultipleController),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Retrieve values from controllers and use them for DCF calculation
+                double netIncome = double.parse(netIncomeController.text);
+                double depreciation = double.parse(depreciationController.text);
+                double amortization = double.parse(amortizationController.text);
+                double capEx = double.parse(capExController.text);
+                double workingCapital =
+                    double.parse(workingCapitalController.text);
+                double costOfEquity = double.parse(costOfEquityController.text);
+                double costOfDebt = double.parse(costOfDebtController.text);
+                double taxRate = double.parse(taxRateController.text);
+                double terminalGrowthRate =
+                    double.parse(terminalGrowthRateController.text);
+                int forecastYears = int.parse(forecastYearsController.text);
+                double exitMultiple = double.parse(exitMultipleController.text);
+
+                // Perform DCF calculation using retrieved values
+                double dcfValue = calculateDCF(
+                    netIncome,
+                    depreciation,
+                    amortization,
+                    capEx,
+                    workingCapital,
+                    costOfEquity,
+                    costOfDebt,
+                    taxRate,
+                    terminalGrowthRate,
+                    forecastYears,
+                    exitMultiple);
+
+                // Print or use the DCF value as needed
+                print('DCF Value: $dcfValue');
+              },
+              child: Text('Calculate DCF'),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-```
 
- 
+  Widget buildTextField(String labelText, TextEditingController controller) {
+    return TextField(
+      controller: controller,
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(labelText: labelText),
+    );
+  }
+
+  // Perform DCF calculation using the provided inputs
+  double calculateDCF(
+      double netIncome,
+      double depreciation,
+      double amortization,
+      double capEx,
+      double workingCapital,
+      double costOfEquity,
+      double costOfDebt,
+      double taxRate,
+      double terminalGrowthRate,
+      int forecastYears,
+      double exitMultiple) {
+    // Implement your DCF calculation logic here
+    // ...
+
+    // For demonstration purposes, a simple calculation is performed
+    double dcfValue = netIncome - capEx;
+    return dcfValue;
+  }
+}
